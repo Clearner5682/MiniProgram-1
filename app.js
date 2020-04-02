9e56cfe9d4dd5b931b666c7ad43b14c46220d6f2
Clearner5682 <625293627@qq.com>
2020-04-02 20:54:39 +0800

    登录流程增加token检查

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//app.js
+const network = require('./utils/network.js');
+
 App({
   onLaunch: function() {
     // 展示本地存储能力
@@ -8,47 +9,42 @@ App({
 
     // 登录
     var token = wx.getStorageSync("token");
-    if (!token) {// 本地没有token，掉登录接口
-      wx.login({
-        success: res => {
-          const code = res.code;
-          wx.request({
-            url: 'http://localhost:5000/api/Auth/LoginWX?code=' + code,
-            method: "POST",
-            success: loginRes => {
-              wx.setStorageSync("token",loginRes.data.token);
-            },
-            fail: loginRes => {
-              console.log(loginRes);
-            }
-          })
-        }
+    if (!token) { // 本地没有token，掉登录接口
+      this.login();
+    } else { // 校验token的有效性？
+      network.request({
+        url: 'http://localhost:5000/api/Auth/CheckToken?token=' + token,
+        method:'POST'
       })
-    }else{// 校验token的有效性？
-
+      .then(res=>{
+        this.globalData.token = loginRes.data.token;// 把token放到全局变量，所有页面都能访问
+        wx.setStorageSync("token", loginRes.data.token);
+      })
+      .catch(res=>{
+        this.login();
+      });
     }
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    token:''
+  },
+  login() {
+    wx.login({
+      success: res => {
+        const code = res.code;
+        wx.request({
+          url: 'http://localhost:5000/api/Auth/LoginWX?code=' + code,
+          method: "POST",
+          success: loginRes => {
+            this.globalData.token=loginRes.data.token;// 把token放到全局变量，所有页面都能访问
+            wx.setStorageSync("token", loginRes.data.token);
+          },
+          fail: loginRes => {
+            console.log(loginRes);
+          }
+        })
+      }
+    })
   }
 })
